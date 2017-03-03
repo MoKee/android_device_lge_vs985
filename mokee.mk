ae9ad6ee3974bf02aab12ae37809b27a70a61079
martincz <martincz@mokeedev.com>
2017-03-04 00:11:39 +0800

    vs985: mokee-ify

--- mokee.mk
+++ mokee.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/vs985/full_vs985.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_vs985
+PRODUCT_NAME := mk_vs985
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
